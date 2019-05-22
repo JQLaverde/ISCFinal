2a9a1434b837a499749e49b2f5aee6edfe206b20
Jonathan Quiroz Laverde <jqlaverde@gmail.com>
2019-05-21 21:36:07 -0500

    RF03 a little

--- fechas.py
+++ fechas.py
@@ -19,9 +19,17 @@ class Fecha():
     """
 
     def __init__(self, fecha):
+        self.__numeroDiasMes = [None, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
 
         self.ano = fecha[0]
+
+        if fecha[1] < 1 or fecha[1] > 12:
+            raise ValueError("El mes debe estar entre 1 y 12")
         self.mes = fecha[1]
+
+        if fecha[2] < 1 or fecha[2] > self.__numeroDiasMes[self.mes]:
+            raise ValueError("El dia debe estar entre 1 y %d"\
+                            %(self.__numeroDiasMes[self.mes]))
         self.dia = fecha[2]
 
         if len(fecha) == 3:
@@ -29,6 +37,41 @@ class Fecha():
             self.minutos = 0
             self.segundos = 0
         elif len(fecha) == 6:
+
+            if fecha[3] < 0 or fecha[3] > 23:
+                raise ValueError("La hora debe estar entre 0 y 23")
             self.hora = fecha[3]
+
+            if fecha[4] < 0 or fecha[4] > 59:
+                raise ValueError("Los minutos debe estar entre 0 y 59")
             self.minutos = fecha[4]
+
+            if fecha[5] < 0 or fecha[5] > 59:
+                raise ValueError("Los segundos debe estar entre 0 y 59")
             self.segundos = fecha[5]
+
+    def __eq__(self, fechaCompara):
+        if self.ano == fechaCompara.ano and\
+           self.mes == fechaCompara.mes and\
+           self.dia == fechaCompara.dia and\
+           self.hora == fechaCompara.hora and\
+           self.minutos == fechaCompara.minutos and\
+           self.segundos == fechaCompara.segundos:
+           return True
+
+        return False
+
+    def sumarAnos(self, numeroAnos):
+        fechaNueva = Fecha((self.ano, self.mes, self.dia))
+        fechaNueva.ano += numeroAnos
+        return fechaNueva
+
+    def sumarMeses(self, numeroMeses):
+        fechaNueva = Fecha((self.ano, self.mes, self.dia))
+        fechaNueva.mes += numeroMeses
+
+        while fechaNueva.mes > 12:
+            fechaNueva.mes -= 12
+            fechaNueva.ano += 1
+
+        return fechaNueva
--- test_fechas.py
+++ test_fechas.py
@@ -11,7 +11,7 @@ class PruebasFechas(unittest.TestCase):
 
     def test_instanciado(self):
         try:
-            fecha = fechas.Fecha((2019, 5, 21))
+            fechas.Fecha((2019, 5, 21))
         except NameError:
             raise AssertionError("La clase fecha no esta definida")
         return True
@@ -34,7 +34,84 @@ class PruebasFechas(unittest.TestCase):
         self.assertEqual(fecha.minutos, 40)
         self.assertEqual(fecha.segundos, 46)
 
+    def test_rango_mes(self):
+        try:
+            fechas.Fecha((2019, 13, 5))
+        except ValueError:
+            return True
+        raise AssertionError("No esta verificando el mes")
 
+    def test_numeros_dias_meses(self):
+        fecha = fechas.Fecha((2019, 3, 5))
+        self.assertEqual(fecha._Fecha__numeroDiasMes[1], 31)
+        self.assertEqual(fecha._Fecha__numeroDiasMes[2], 28)
+        self.assertEqual(fecha._Fecha__numeroDiasMes[3], 31)
+        self.assertEqual(fecha._Fecha__numeroDiasMes[4], 30)
+        self.assertEqual(fecha._Fecha__numeroDiasMes[5], 31)
+        self.assertEqual(fecha._Fecha__numeroDiasMes[6], 30)
+        self.assertEqual(fecha._Fecha__numeroDiasMes[7], 31)
+        self.assertEqual(fecha._Fecha__numeroDiasMes[8], 31)
+        self.assertEqual(fecha._Fecha__numeroDiasMes[9], 30)
+        self.assertEqual(fecha._Fecha__numeroDiasMes[10], 31)
+        self.assertEqual(fecha._Fecha__numeroDiasMes[11], 30)
+        self.assertEqual(fecha._Fecha__numeroDiasMes[12], 31)
+
+    def test_rango_dia(self):
+        for mes in range(1, 13):
+            try:
+                fechaAux = fechas.Fecha((2019, 3, 5))
+                fechas.Fecha((2019, mes, fechaAux._Fecha__numeroDiasMes[mes] + 1))
+            except ValueError:
+                continue
+            raise AssertionError("No esta verificando el dia")
+
+    def test_rango_hora(self):
+        try:
+            fechas.Fecha((2019, 3, 5, 24, 0, 0))
+        except ValueError:
+            return True
+        raise AssertionError("No esta verificando la hora")
+
+    def test_rango_minutos(self):
+        try:
+            fechas.Fecha((2019, 3, 5, 23, 60, 0))
+        except ValueError:
+            return True
+        raise AssertionError("No esta verificando los minutos")
+
+    def test_rango_segundos(self):
+        try:
+            fechas.Fecha((2019, 3, 5, 23, 10, 60))
+        except ValueError:
+            return True
+        raise AssertionError("No esta verificando los segundos")
+
+    def test_compara_fechas(self):
+        fechaInicial = fechas.Fecha((2019, 3, 5, 5, 5, 5))
+        fechaCompara = fechas.Fecha((2019, 3, 5, 5, 5, 5))
+        self.assertTrue(fechaInicial == fechaCompara)
+
+    def test_suma_anos(self):
+        fecha = fechas.Fecha((2019, 5, 19))
+        fechaNueva = fecha.sumarAnos(1)
+        self.assertTrue(fechaNueva == fechas.Fecha((2020, 5, 19)))
+
+    def test_sumar_pocos_meses(self):
+        fecha = fechas.Fecha((2019, 5, 19))
+        fechaNueva = fecha.sumarMeses(4)
+        self.assertTrue(fechaNueva == fechas.Fecha((2019, 9, 19)))
+
+    def test_sumar_meses_pasando_ano(self):
+        fecha = fechas.Fecha((2019, 5, 19))
+        fechaNueva = fecha.sumarMeses(12)
+        self.assertTrue(fechaNueva == fechas.Fecha((2020, 5, 19)))
+
+    def test_sumar_meses_pasando_anos(self):
+        fecha = fechas.Fecha((2019, 5, 19))
+        fechaNueva = fecha.sumarMeses(27)
+        self.assertTrue(fechaNueva == fechas.Fecha((2021, 8, 19)))
+
+    
 
 if __name__=='__main__':
     unittest.main()
